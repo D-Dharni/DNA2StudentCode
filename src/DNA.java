f32f2e8c772495bb28c4d3c944072dd69bbf0d1c
devendharni <deven.dharni@gmail.com>
2025-10-19 16:24:59 -0600

    -Thought about a basic algorithm -Wrote out pseudocode for it

--- src/DNA.java
+++ src/DNA.java
@@ -6,7 +6,7 @@
  * at Menlo School in Atherton, CA
  *</p>
  * <p>
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Deven Dharni
  *</p>
  */
 
@@ -16,7 +16,27 @@ public class DNA {
      * TODO: Complete this function, STRCount(), to return longest consecutive run of STR in sequence.
      */
     public static int STRCount(String sequence, String STR) {
+        // Create an array to store the numbers for each letter in sequence
 
+        // Make variable to keep track of the maximum number of counts
+
+        // Go through the sequence from the back to the front
+
+            // Check if the substring at that position matches the STR
+
+            // If it does match:
+
+                // Look ahead to see if another repeat is after
+
+                // If there is then set the index equal to 1 + the number after
+
+                // Otherwise set it to 1
+
+                // Update the maximum count if the value is bigger
+
+            // If no match set it equal to 0 at the index
+
+        // Return whatever the maximum count was
         return 0;
     }
 }
